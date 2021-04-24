a393d41060b0e7076d4fd2e4b52f691f6bd7a040
Valdir Torres Borges <valdyrtorres@yahoo.com.br>
2021-04-24 00:10:52 -0300

    Microservico auth finalizado

--- pagamento/src/main/java/com/valdirtorres/pagamento/jwt/JwtTokenProvider.java
+++ pagamento/src/main/java/com/valdirtorres/pagamento/jwt/JwtTokenProvider.java
@@ -1,6 +1,7 @@
-package com.valdirtorres.auth.jwt;
+package com.valdirtorres.pagamento.jwt;
 
 import java.util.Base64;
+import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 
@@ -12,6 +13,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
@@ -28,36 +30,52 @@ public class JwtTokenProvider {
 	@Value("${security.jwt.token.secret-key}")
 	private String secretKey;
 	
-	@Value("${security.jwt.token.expire-length}")
-	private String expire;	
-	
-	@Qualifier("userService")
-	@Autowired
-	private UserDetailsService userDetailsService;
-	
 	@PostConstruct
 	protected void init() {
 		secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
 	}
 	
-	public String createToken(String username, List<String> roles) {
-		Claims claims = Jwts.claims().setSubject(username);
-		claims.put("roles", roles);
-		
-		Date now = new Date();
-		Date validate = new Date(now.getTime() + Long.parseLong(expire));
-		
-		return Jwts.builder().setClaims(claims).setIssuedAt(now).setExpiration(validate)
-				.signWith(SignatureAlgorithm.HS256, secretKey).compact();
-	}
-	
 	public Authentication getAuthentication(String token) {
-		UserDetails userDetails = this.userDetailsService.loadUserByUsername(getUserName(token));
+		UserDetails userDetails = new UserDetails() {
+			
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public boolean isEnabled() {
+				return true;
+			}
+			
+			@Override
+			public boolean isCredentialsNonExpired() {
+				return true;
+			}
+			
+			@Override
+			public boolean isAccountNonLocked() {
+				return true;
+			}
+			
+			@Override
+			public boolean isAccountNonExpired() {
+				return true;
+			}
+			
+			@Override
+			public String getUsername() {
+				return "";
+			}
+			
+			@Override
+			public String getPassword() {
+				return "";
+			}
+			
+			@Override
+			public Collection<? extends GrantedAuthority> getAuthorities() {
+				return null;
+			}
+		};
 		return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
-	}
-	
-	private String getUserName(String token) {
-		return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
 	}
 	
 	public String resolveToken(HttpServletRequest req) {
